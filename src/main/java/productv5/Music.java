7a569e1a35f21f2e52b1e091cf7b4b10ced3b613
g00399850 <g00399850@atu.ie>
2023-02-02 11:52:12 +0000

    write code for Music

--- src/main/java/productv5/Music.java
+++ src/main/java/productv5/Music.java
@@ -1,4 +1,35 @@
 package productv5;
 
 public class Music {
+
+    private double price;
+    private String description;
+    private String code;
+    private String artist;
+    private String label;
+    protected static int count = 0;
+
+    public Music(){
+        price = 0.0;
+        description = "";
+        code = "";
+        artist = "";
+        label = "";
+        count++;
+    }
+
+    public void setPrice(double price){ this.price = price; }
+    public double getPrice(){ return price; }
+
+    public void setDescription(String description){ this.description = description; }
+    public String getDescription(){ return description; }
+
+    public void setCode(String code){ this.code = code; }
+    public String getCode(){ return code; }
+
+    public void setArtist(String artist){ this.artist = artist; }
+    public String getArtist(){ return artist; }
+
+    public void setLabel(String label){ this.label = label; }
+    public String getLabel(){ return label; }
 }
